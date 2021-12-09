73fbc08a30f5949637b95f777d879515bb3e385e
Deshan Vimukthi <deshanvimukthi59@gmail.com>
2021-12-09 22:12:42 +0530

    Home Image

--- Screens/Home.js
+++ Screens/Home.js
@@ -1,12 +1,19 @@
 import React from "react";
-import { StyleSheet, Text, TouchableOpacity, View } from "react-native";
+import { Image, StyleSheet, Text, TouchableOpacity, View } from "react-native";
 import Title from "./Components/Title";
 
 const Home = () => {
   return (
     <View>
       <Title />
-      <View></View>
+      <View>
+        <Image
+          source={{
+            uri: "https://iconscout.com/illustration/qa-service-3678715",
+          }}
+          style={styles.banner}
+        />
+      </View>
       <TouchableOpacity>
         <Text>Start</Text>
       </TouchableOpacity>
@@ -16,4 +23,9 @@ const Home = () => {
 
 export default Home;
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  banner: {
+    height: 300,
+    width: 300,
+  },
+});
