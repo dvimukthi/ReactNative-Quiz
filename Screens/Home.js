3ef3b22dd53a49b488dfa97204b99addbf5e4e37
Deshan Vimukthi <deshanvimukthi59@gmail.com>
2021-12-09 21:40:04 +0530

    TouchableOpacity

--- Screens/Home.js
+++ Screens/Home.js
@@ -1,11 +1,15 @@
 import React from "react";
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, Text, TouchableOpacity, View } from "react-native";
 import Title from "./Components/Title";
 
 const Home = () => {
   return (
     <View>
       <Title />
+      <View></View>
+      <TouchableOpacity>
+        <Text>Start</Text>
+      </TouchableOpacity>
     </View>
   );
 };
