d9cd98e12814bbc64c1d0a0ec58b5d497db06b6e
Deshan Vimukthi <deshanvimukthi59@gmail.com>
2021-12-21 19:06:56 +0530

    stack.screen Settings

--- navigation/index.js
+++ navigation/index.js
@@ -8,6 +8,7 @@ function MyStack() {
       <Stack.Screen name="Home" component={Home} />
       <Stack.Screen name="Notifications" component={Notifications} />
       <Stack.Screen name="Profile" component={Profile} />
+      <Stack.Screen name="Settings" component={Settings} />
     </Stack.Navigator>
   );
 }
