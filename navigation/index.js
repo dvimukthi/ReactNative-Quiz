aea7475be27ad51ade5c6a223480470883524afb
Deshan Vimukthi <deshanvimukthi59@gmail.com>
2021-12-21 19:15:09 +0530

    stack Result

--- navigation/index.js
+++ navigation/index.js
@@ -1,6 +1,7 @@
 import { createStackNavigator } from "@react-navigation/stack";
 import Home from "../Screens/Home";
 import Quiz from "../Screens/Quiz";
+import Result from "../Screens/Result";
 
 const Stack = createStackNavigator();
 
@@ -9,7 +10,7 @@ function MyStack() {
     <Stack.Navigator>
       <Stack.Screen name="Home" component={Home} />
       <Stack.Screen name="Quiz" component={Quiz} />
-      <Stack.Screen name="Profile" component={Profile} />
+      <Stack.Screen name="Result" component={Result} />
       <Stack.Screen name="Settings" component={Settings} />
     </Stack.Navigator>
   );
